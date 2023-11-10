fc4dac947eb82d4096fd897b8ca6f7874c77f39a
David <darwu@ucsc.edu>
2023-11-09 17:05:15 -0800

    initial setup for view operations

---
+++ backend/view_store.py
@@ -0,0 +1,14 @@
+from flask import jsonify
+import json
+
+# initializing store for view
+views = {"view": []}
+
+def put(addr):
+    return False
+
+def get(addr):
+    return False
+
+def delete(addr):
+    return False
