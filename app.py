180f286a2973d037a2b1e285ef5f75a89e41d330
Hunter Tratar <hunterrrisatratar@gmail.com>
2023-11-09 22:21:30 -0800

    New arch dropped 🗣️🗣️🗣️🗣️🗣️🗣️🗣️🗣️🔥🔥🔥🔥 Got rid of threading and socketing (even though i love it so much) and added a system that when a request is made from a client, the request gets processed normally and then sends a broadcasting request to an alternate endpoint called /kvs/broadcast/<key>. This takes the data and adds a new field called updated-metadata and processes and updates the other replicas accordingly without sending duplciated requests.

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 from flask import Flask, request
 from backend.kvs_store import put,get,delete
-from backend.communication import communication_thread, broadcast, real_broadcast
-import threading
+from backend.communication import broadcast
+from backend.update_metadata import print_metadata
 import os
 app = Flask(__name__)
 
 @app.route('/kvs/<key>', methods=['PUT','GET','DELETE'])
 def update(key):
-    # write an if output == 503, sleep 1 seconds and retry request shrug
     if request.method == 'PUT':
         data = request.get_json()
         output = put(data, key)
-        real_broadcast(key, data, request.method)
+        broadcast(key, data, request.method, output) # might need to run this in another thread
     elif request.method == 'GET':
         data = request.get_json()
         output = get(data, key)
-        real_broadcast(key, data, request.method)
+        broadcast(key, data, request.method, output)
     elif request.method == 'DELETE':
         data = request.get_json()
         output = delete(data, key)
-        real_broadcast(key, data, request.method)
+        broadcast(key, data, request.method, output)
     return output
 
 @app.route('/kvs/broadcast/<key>', methods=['PUT','GET','DELETE'])
@@ -36,10 +35,6 @@ def update_repliace(key):
     return output
 
 if __name__ == "__main__":
-    t = threading.Thread(target=communication_thread)
-    t.daemon = True
-    t.start()
-
     this_ip = os.environ.get("SOCKET_ADDRESS")
     ip, port = this_ip.split(":")
     app.run(host=ip, port=port)
--- backend/communication.py
+++ backend/communication.py
@@ -1,63 +1,19 @@
 import os
-import socket
-import threading
-from .kvs_store import check_metadata, update_store
-from .update_metadata import update_metadata
+import requests
 import json
 
-def broadcast(key, output, data, request):
+def broadcast(key, data, request, output):
     if output[1] == 200 or output[1] == 201:
+        output = json.loads(output[0])
+        data['broadcasted-metadata'] = output['casual-metadata']
         ip_list = os.environ.get("VIEW").split(",")
         this_ip = os.environ.get("SOCKET_ADDRESS")
         ip_list.remove(this_ip)
         for ip in ip_list:
-            ip_address, port = ip.split(":")
-            clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            clientSocket.connect((ip_address, 8100))
-            payload = {
-                "key":key,
-                "request-type":request,
-                "request":data,
-                "response": json.loads(output[0])
-            }
-            payload = json.dumps(payload)
-            clientSocket.send(payload.encode())
-
-
-def handle_client(client_socket):
-    data = client_socket.recv(1024)
-    data = data.decode()
-    data = json.loads(data)
-    isUpToDate = check_metadata(data['request']['casual-metadata'])
-    if isUpToDate == True:
-        if data['request-type'] == 'PUT':
-            update_store(data['key'], data['request-type'], data['request']['value'])
-            update_metadata(data['response']['casual-metadata'])
-            client_socket.close()
-        elif data['request-type'] == 'GET':
-            update_metadata(data['response']['casual-metadata'])
-            client_socket.close()
-        elif data['request-type'] == 'DELETE':
-            update_store(data['key'], data['request-type'])
-            update_metadata(data['response']['casual-metadata'])
-            client_socket.close()
-    else:
-        # sleep one second and retry the request shrug
-        print("Not to up date sorry :/")
-
-
-def communication_thread():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    this_ip = os.environ.get("SOCKET_ADDRESS")
-    ip, port = this_ip.split(":")
-    ADDR = (ip,8100)
-    print(ADDR)
-    server_socket.bind(ADDR)
-    server_socket.listen(1)
-    print(f"Listening for connection on {ADDR}...")
-
-    while True:
-        client_socket, client_address = server_socket.accept()
-        # print(f"Accepted connection from {client_address}")
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
-        client_handler.start()
+            url = f"http://{ip}/kvs/broadcast/{key}"
+            if request == 'PUT':
+                response = requests.put(url, json=data, timeout=5)
+            elif request == 'GET':
+                response = requests.get(url, json=data, timeout=5)
+            elif request == 'DELETE':
+                response = requests.delete(url, json=data, timeout=5)
