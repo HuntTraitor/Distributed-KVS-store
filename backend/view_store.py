d124c03aca38206e13c6f52e49b7f9d94388bbf9
David <darwu@ucsc.edu>
2023-11-11 15:31:19 -0800

    replaced view_store[] with VIEW env; removed get() param

--- backend/view_store.py
+++ backend/view_store.py
@@ -1,22 +1,25 @@
 from flask import jsonify
-import json
+import os
 
-# initializing store for view
-view_store = []
+# get VIEW as a list
+view = os.environ.get("VIEW").split(" ")
 
 def put(addr): # assumes addr is in the form: <IP:PORT> as dealt with in app.py
-    if addr in view_store:
+    if addr in view:
         return jsonify({"result": "already present"}), 200
     else:
-        view_store.append(addr)
+        view.append(addr)
+        os.environ["VIEW"] = ",".join(view)
         return jsonify({"result": "added"}), 201
 
-def get(addr):
-    return jsonify({"view": view_store}), 200
+def get():
+    return jsonify({"view": view}), 200
 
 def delete(addr):
-    if addr in view_store:
-        view_store.remove(addr)
+    if addr in view:
+        view.remove(addr)
+        os.environ["VIEW"] = ",".join(view)
         return jsonify({"result": "deleted"}), 200
     else:
         return jsonify({"error": "View has no such replica"}), 404
+
